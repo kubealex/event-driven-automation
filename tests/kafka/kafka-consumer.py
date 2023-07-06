341edbcec5bee0e675a50f286bc78bd690cc94be
kubealex <al.rossi87@gmail.com>
2023-07-06 09:43:56 +0200

    cleaning up

--- tests/kafka/kafka-consumer.py
+++ tests/kafka/kafka-consumer.py
@@ -3,8 +3,7 @@ import asyncio
 
 async def consume():
     consumer = AIOKafkaConsumer(
-        "eda-topic", bootstrap_servers='<IP/HOST PLACEHOLDER>:9092')
-    # Get cluster layout and topic/partition allocation
+        "eda-topic", bootstrap_servers='KAFKA_HOST:9092')
     await consumer.start()
     try:
         async for msg in consumer:
--- tests/kafka/kafka-sender.py
+++ tests/kafka/kafka-sender.py
@@ -4,10 +4,8 @@ import asyncio
 
 async def produce():
     producer = AIOKafkaProducer(
-        bootstrap_servers='<IP/HOST PLACEHOLDER>:9092', value_serializer=lambda m: json.dumps(m).encode('utf-8'))
-    # Get cluster layout and topic/partition allocation
+        bootstrap_servers='KAFKA_HOST:9092', value_serializer=lambda m: json.dumps(m).encode('utf-8'))
     await producer.start()
-    # Produce messages
     await producer.send("eda-topic", {'name': 'greeting', 'message': 'TEST'})
     await producer.stop()
 asyncio.run(produce())
